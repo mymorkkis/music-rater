964b2ae8a71cdbc37e509d1012a1f87f5d78c03d
Max <mveasey87@gmail.com>
2019-05-18 07:57:38 +0100

    Rename dbal_repositories in tests for clarity

--- tests/test_dbal_repository.py
+++ tests/test_dbal_repository.py
@@ -7,14 +7,14 @@ from src.dbal.models.genre import Genre
 
 
 @pytest.fixture
-def dbal_repository(test_session):
+def genre_repository(test_session):
     return DBALRepository(model=Genre, db_session=test_session)
 
 
 @pytest.fixture
-def stored_genre(dbal_repository):
+def stored_genre(genre_repository):
     genre = Genre(name='Rock')
-    stored_genre = dbal_repository.add(genre)
+    stored_genre = genre_repository.add(genre)
     return stored_genre
 
 
@@ -24,68 +24,68 @@ class TestDBALRepsoitory:
         assert stored_genre.id is not None
         assert stored_genre.name == 'Rock'
 
-    def test_can_get_an_entity_by_id(self, stored_genre, dbal_repository):
-        genre = dbal_repository.get_by_id(entity_id=stored_genre.id)
+    def test_can_get_an_entity_by_id(self, stored_genre, genre_repository):
+        genre = genre_repository.get_by_id(entity_id=stored_genre.id)
         assert genre == stored_genre
 
-    def test_fetching_a_non_existent_entity_raises_no_result_found(self, dbal_repository):
+    def test_fetching_a_non_existent_entity_raises_no_result_found(self, genre_repository):
         with pytest.raises(NoResultFound):
-            dbal_repository.get_by_id(entity_id=999)
+            genre_repository.get_by_id(entity_id=999)
 
-    def test_can_find_an_entity_by_attribute(self, stored_genre, dbal_repository):
-        genres = dbal_repository.find(attribute='name', value='Rock')
+    def test_can_find_an_entity_by_attribute(self, stored_genre, genre_repository):
+        genres = genre_repository.find(attribute='name', value='Rock')
         assert genres[0].id == stored_genre.id
 
     def test_can_find_multiple_entities_by_attribute(self, test_session):
-        dbal_repository = DBALRepository(model=Artist, db_session=test_session)
-        blur = dbal_repository.add(Artist(name='Blur', artist_type='group', genre_id=1))
-        oasis = dbal_repository.add(Artist(name='Oasis', artist_type='group', genre_id=1))
-        genres = dbal_repository.find(attribute='artist_type', value='group')
+        artist_repository = DBALRepository(model=Artist, db_session=test_session)
+        blur = artist_repository.add(Artist(name='Blur', artist_type='group', genre_id=1))
+        oasis = artist_repository.add(Artist(name='Oasis', artist_type='group', genre_id=1))
+        genres = artist_repository.find(attribute='artist_type', value='group')
 
         assert set(genres) == {blur, oasis}
 
-    def test_empty_list_returned_if_no_entities_found(self, dbal_repository):
-        genres = dbal_repository.find(attribute='name', value='Non Existing')
+    def test_empty_list_returned_if_no_entities_found(self, genre_repository):
+        genres = genre_repository.find(attribute='name', value='Non Existing')
         assert genres == []
 
-    def test_attribute_error_raised_if_unknown_attrubute_passed_in_to_find(self, dbal_repository):
+    def test_attribute_error_raised_if_unknown_attrubute_passed_in_to_find(self, genre_repository):
         with pytest.raises(AttributeError):
-            dbal_repository.find(attribute='boom', value='BOOM')
+            genre_repository.find(attribute='boom', value='BOOM')
 
-    def test_can_delete_an_entity(self, stored_genre, dbal_repository):
-        dbal_repository.delete(stored_genre)
+    def test_can_delete_an_entity(self, stored_genre, genre_repository):
+        genre_repository.delete(stored_genre)
         with pytest.raises(NoResultFound):
-            dbal_repository.get_by_id(entity_id=stored_genre.id)
+            genre_repository.get_by_id(entity_id=stored_genre.id)
 
-    def test_attempt_to_delete_a_non_existent_entity_does_not_raise_error(self, dbal_repository):
+    def test_attempt_to_delete_a_non_existent_entity_does_not_raise_error(self, genre_repository):
         genre = Genre(name='Not Stored')
-        dbal_repository.delete(genre)
+        genre_repository.delete(genre)
 
-    def test_can_update_an_entity(self, stored_genre, dbal_repository):
+    def test_can_update_an_entity(self, stored_genre, genre_repository):
         stored_genre.name = 'Punk'
-        updated_genre = dbal_repository.update(stored_genre)
-        stored_updated_genre = dbal_repository.get_by_id(entity_id=updated_genre.id)
+        updated_genre = genre_repository.update(stored_genre)
+        stored_updated_genre = genre_repository.get_by_id(entity_id=updated_genre.id)
         assert updated_genre.name == 'Punk'
         assert stored_updated_genre.name == 'Punk'
         assert stored_updated_genre.id == stored_genre.id
 
-    def test_upsert_creates_an_entity_if_none_found(self, dbal_repository):
+    def test_upsert_creates_an_entity_if_none_found(self, genre_repository):
         new_genre = Genre(name='Dance')
-        stored_genre = dbal_repository.upsert(new_genre)
+        stored_genre = genre_repository.upsert(new_genre)
         assert stored_genre.id is not None
         assert stored_genre.name == 'Dance'
 
-    def test_upsert_updates_an_entity_if_found(self, stored_genre, dbal_repository):
+    def test_upsert_updates_an_entity_if_found(self, stored_genre, genre_repository):
         assert stored_genre.name == 'Rock'
         stored_genre.name = 'Punk'
-        updated_entity = dbal_repository.upsert(stored_genre)
-        stored_updated_entity = dbal_repository.get_by_id(entity_id=updated_entity.id)
+        updated_entity = genre_repository.upsert(stored_genre)
+        stored_updated_entity = genre_repository.get_by_id(entity_id=updated_entity.id)
         assert updated_entity.name == 'Punk'
         assert stored_updated_entity.name == 'Punk'
         assert stored_updated_entity.id
 
-    def test_can_truncate_table(self, stored_genre, dbal_repository):
-        genre_rows = dbal_repository.session.query(dbal_repository.model)
+    def test_can_truncate_table(self, stored_genre, genre_repository):
+        genre_rows = genre_repository.session.query(genre_repository.model)
         assert len(genre_rows.all()) == 1
-        dbal_repository.truncate_table()
+        genre_repository.truncate_table()
         assert len(genre_rows.all()) == 0
